41642acae3a31f0e3037ca03d93e5c01738633fa
Erin Stalzer <erinstalzer@gmail.com>
2019-05-13 03:54:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,6 +21,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student = {}
     doc = Nokogiri::HTML(File.read(profile_url))
+    binding.pry
     doc.css("div.social-icon-container.a").each do |link|
       if link.include?("twitter")
         student[:twitter] = link.css("a").attribute('href').value

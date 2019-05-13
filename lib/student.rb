b3a1d269345f8728eee3c14d312039999b5c33f3
Erin Stalzer <erinstalzer@gmail.com>
2019-05-13 04:23:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    self[:twitter] = attributes_hash[:twitter]
+    self.store(:twitter) = attributes_hash[:twitter]
     self[:bio] = attributes_hash[:bio]
     student[:linkedin] = attributes_hash[:linkedin]
     student[:blog] = attributes_hash[:blog]
